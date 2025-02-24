8fcd3f4cd46d1bf486472a1439535f7cc40f3ddd
Kunkuma Geeth <kunkumagp@gmail.com>
2025-02-24 17:53:01 +0530

    .

--- client.js
+++ client.js
@@ -1,6 +1,10 @@
 // Replace with your WebSocket server URL
 const clientSocket = new WebSocket("ws://localhost:8080");
 
+const scriptButton = document.getElementById("scriptRun");
+let isBotRunning = false;
+
+
 clientSocket.onopen = function(event) {
     console.log("WebSocket connection established.");
 //  document.getElementById("messages").innerHTML += "<p>Connected to server.</p>";
@@ -9,8 +13,6 @@ clientSocket.onopen = function(event) {
 clientSocket.onmessage = function(event) {
 
     let eventDataObject = JSON.parse(event.data);
-
-    // console.log('event : ', eventDataObject);
 
     if(eventDataObject.msg_type == "authorize"){
         setInitData(eventDataObject);
@@ -21,13 +23,20 @@ clientSocket.onmessage = function(event) {
     }
 
     if(eventDataObject.msg_type == "trade_open"){
-        // console.log("eventDataObject: ", eventDataObject);
-        setResultNotification(eventDataObject.data.lastTradeDetails);
+        setResultNotification(eventDataObject.data.lastTradeDetails, 'create');
+    }
+
+    if(eventDataObject.msg_type == "tick_counter"){
+        setTickCountDown(eventDataObject.data.tick_count, eventDataObject.data.length);
+    }
+
+    if(eventDataObject.msg_type == "flash_message"){
+        setFlashNotification(eventDataObject.data.message, eventDataObject.data.time);
+    }
 
 
-        setTimeout(() => {
-            // fetchTradeDetails(lastTradeId);
-        }, 500);
+    if(eventDataObject.msg_type == "trade_closed"){
+        setResultNotification(eventDataObject.data.lastTradeDetails, 'update');
     }
 
     // document.getElementById("messages").innerHTML += "<p>Server: " + event.data + "</p>";
@@ -58,22 +67,40 @@ function authenticate() {
 }
 
 function botStart() {
-    const message = "Bot starting...";
-    console.log("Message sent: " + message);
 
-    let dataObj = {
-        type: 'start', 
-        params:{
-            market: market
-        }
-    };
+    if(!isBotRunning){
+        scriptButton.innerHTML = "Click to stop Bot";
+        const message = "Bot starting...";
+        console.log("Message sent: " + message);
+    
+        let dataObj = {
+            type: 'start', 
+            params:{
+                market: market
+            }
+        };
+        isBotRunning = true;
+    
+        clientSocket.send(JSON.stringify(dataObj));
+    } else {
+        scriptButton.innerHTML = "Start Bot";
+        const message = "Bot stoped";
+        console.log("Message sent: " + message);
+        let dataObj = {
+            type: 'stop'
+        };
+        isBotRunning = false;
 
-    clientSocket.send(JSON.stringify(dataObj));
+        clientSocket.send(JSON.stringify(dataObj));
+    }
+
+
+    
 }
 
 function setInitData(data) {
     if(data.msg_type == "authorize"){
-        console.log(data);
+        console.log('data: ',data);
 
         authenticateButton.innerHTML = "Authenticated. Ready to trade.";
         authenticateButton.disabled = true;
@@ -93,52 +120,85 @@ function setInitData(data) {
     }
 }
 
-function setResultNotification(lastTradeDetails){
+function setResultNotification(lastTradeDetails, type){
 
     const marketObj = marketArray.find((item) => item.value === lastTradeDetails.market);
-    const element = document.getElementById(lastTradeDetails.id);
+    let newClassName = null;
+    let status = null;
 
-    if (element) {
-        let newClassName = null;
-        let status = null;
+    if (type == 'update') {
+        let profit = lastTradeDetails.profit;
+        const element = document.getElementById(lastTradeDetails.id);
 
-        if (profit >= 0) {
-            newClassName = "green";
-            status = "WIN";
-        } else if (profit < 0) {
-            newClassName = "red";
-            status = "LOSS";
-        }
-
-        const profitElement = document.getElementById(lastTradeDetails.id + "-profit");
-        const statusElement = document.getElementById(lastTradeDetails.id + "-status");
-
-        if (profitElement) {
-            const spanElement = profitElement.querySelector("span"); // Select the <span> inside the parent element
-            if (spanElement) {
-                spanElement.className = newClassName; // Set the class
-                spanElement.innerHTML = profit; // Set the inner HTML
+        if(element){
+            if (profit >= 0) {
+                newClassName = "green";
+                status = "WIN";
+            } else if (profit < 0) {
+                newClassName = "red";
+                status = "LOSS";
+            }
+    
+            const profitElement = document.getElementById(lastTradeDetails.id + "-profit");
+            const statusElement = document.getElementById(lastTradeDetails.id + "-status");
+    
+            if (profitElement) {
+                const spanElement = profitElement.querySelector("span"); // Select the <span> inside the parent element
+                if (spanElement) {
+                    spanElement.className = newClassName; // Set the class
+                    spanElement.innerHTML = profit; // Set the inner HTML
+                } else {
+                    console.log("No <span> element found inside the parent element.");
+                }
             } else {
-                console.log("No <span> element found inside the parent element.");
+                console.log("Parent element not found.");
+            }
+    
+            if (statusElement) {
+                const spanElement = statusElement.querySelector("span"); // Select the <span> inside the parent element
+                if (spanElement) {
+                    spanElement.className = newClassName; // Set the class
+                    spanElement.innerHTML = status; // Set the inner HTML
+                } else {
+                    console.log("No <span> element found inside the parent element.");
+                }
+            } else {
+                console.log("Parent element not found.");
             }
         } else {
-            console.log("Parent element not found.");
-        }
-
-        if (statusElement) {
-            const spanElement = statusElement.querySelector("span"); // Select the <span> inside the parent element
-            if (spanElement) {
-                spanElement.className = newClassName; // Set the class
-                spanElement.innerHTML = status; // Set the inner HTML
-            } else {
-                console.log("No <span> element found inside the parent element.");
+            if(lastTradeDetails.profit > 0){
+                newClassName = "green";
+                status = "WIN";
+            } else if(lastTradeDetails.profit < 0){
+                newClassName = "red";
+                status = "LOSS";
             }
-        } else {
-            console.log("Parent element not found.");
+            $(".result-notification").prepend(`<span class="stake-info" id="${lastTradeDetails.id}"><span class="detailt"><span>Contract ID : </span><span class="contract-info">${lastTradeDetails.id}</span></span><span class="detailt"><span>Market : </span><span class="contract-info">${marketObj.name}</span></span><span class="detailt"><span>Type : </span><span class="contract-info">${lastTradeDetails.type}</span></span><span class="detailt"><span>Stake : </span><span class="contract-info">${lastTradeDetails.stake}</span></span><span class="detailt"><span>Profit / Loss Amount : </span><span class="contract-info" id="${lastTradeDetails.id}-profit"><span class="${newClassName}">${lastTradeDetails.profit}</span></span></span><span class="detailt"><span>Status : </span><span class="contract-info" id="${lastTradeDetails.id}-status"><span class="${newClassName}">${status}</span></span></span></span>`);
+        
         }
+        
     } else {
         $(".result-notification").prepend(`<span class="stake-info" id="${lastTradeDetails.id}"><span class="detailt"><span>Contract ID : </span><span class="contract-info">${lastTradeDetails.id}</span></span><span class="detailt"><span>Market : </span><span class="contract-info">${marketObj.name}</span></span><span class="detailt"><span>Type : </span><span class="contract-info">${lastTradeDetails.type}</span></span><span class="detailt"><span>Stake : </span><span class="contract-info">${lastTradeDetails.stake}</span></span><span class="detailt"><span>Profit / Loss Amount : </span><span class="contract-info" id="${lastTradeDetails.id}-profit"><span class="">-</span></span></span><span class="detailt"><span>Status : </span><span class="contract-info" id="${lastTradeDetails.id}-status"><span class="">-</span></span></span></span>`);
     }
 
 
+}
+
+function setTickCountDown(tickCount, tick) {
+    if (tickCount > tick) {
+        setFlashNotification(`Trade will close in <span class="number">${tickCount - tick}</span> tick.`,0);
+    } else if (tickCount == tick) {
+        setFlashNotification(``, 0);
+    }
+}
+
+function setFlashNotification(message, timeInSeconds) {
+    if(isBotRunning){
+        $(".flash-notification").html(message);
+        if (timeInSeconds > 0) {
+            setTimeout(() => {
+                $(".flash-notification").html("");
+            }, timeInSeconds * 1000);
+        }
+    }
 }
--- main.js
+++ main.js
@@ -4,25 +4,35 @@ const webSocket = require("ws");
 let ws = new webSocket("wss://ws.binaryws.com/websockets/v3?app_id=1089");
 
 const server = new webSocket.Server({ port: 8080 });
+const martingaleMultiplier = 2.07112;
 
 let socket = null;
+let botStartStatus = false;
+let botStopStatus = true;
 
 let isTradeRunning = false, automation = false;
 let tradeType = "even";
 
 let tradeProposal = null;
-
-// let netProfit = 0;
 let tickCount = 1;
+let stopTimer = false;
 
 let varObject = {
     initianAccountBalance: 0,
     amountPutForTrading: 0,
+    targetAmount: 0,
     amountPercentage: 0.1,
+    targetPercentage:0.08,
     updatedAccountBalance: 0,
     stake: 0.35,
     market: null,
     totalTradeCount: 0,
+    winTradeCount: 0,
+    lossTradeCount: 0,
+    lostCountInRow: 0,
+    currentProfitAmount: 0,
+    currentLossAmount: 0,
+    netProfit: 0,
     lastTradeDetails: {
         id: null,
         type: null,
@@ -48,6 +58,11 @@ server.on("connection", (sc) => {
         if (res.type === 'start') {
             botStart(res.params);
             // getAuthentication();
+        }
+
+        if (res.type === 'stop') {
+            console.log(11223);
+            botStop();
         }
     });
 
@@ -80,7 +95,7 @@ ws.onopen = function () {
 
 ws.onmessage = function (event) {
     wsResponse = JSON.parse(event.data);
-    console.log("wsResponse: ", wsResponse);
+    // console.log("wsResponse: ", wsResponse);
 
     if(wsResponse.msg_type == "authorize"){
         setInitData(wsResponse);
@@ -89,7 +104,7 @@ ws.onmessage = function (event) {
 
     if(wsResponse.msg_type == "proposal"){
         tradeProposal = wsResponse;
-        if(!isTradeRunning){
+        if(!isTradeRunning && botStartStatus){
             makeTheTrade();
         }
     }
@@ -104,16 +119,69 @@ ws.onmessage = function (event) {
         }
 
         automation = true;
-        socket.send(JSON.stringify({msg_type: 'trade_open',data: varObject}));
 
-        // setTimeout(() => {
-        //     fetchTradeDetails(lastTradeId);
-        // }, 500);
+        setTimeout(() => {
+            fetchTradeDetails(varObject.lastTradeDetails.id);
+        }, 500);
+        socket.send(JSON.stringify({msg_type: 'trade_open',data: varObject}));
 
     }
 
     if (wsResponse.msg_type === "proposal_open_contract") {
-        console.log('wsResponse: ', wsResponse);
+        // console.log('wsResponse: ', wsResponse);
+
+        if (wsResponse.proposal_open_contract.contract_id === varObject.lastTradeDetails.id) {
+            const contract = wsResponse.proposal_open_contract;
+
+            if (contract.is_sold){
+                const profit = contract.profit;
+                const result = profit > 0 ? "Win" : "Loss";
+
+                // setInfo(contract, profit);
+                stakeChange(result);
+                varObject.lastTradeDetails.profit = profit;
+
+                updateReturnDataObject(profit);
+
+
+                if (varObject.currentLossAmount < 0) {
+                    if(varObject.lostCountInRow >= 2){
+                        // let newTime = (getRandomNumber(1, 2) * 60000 );
+                        let newTime = (getRandomNumber(10, 20) * 1000);
+                        setTimer(newTime);
+                        setTimeout(() => {
+                            isTradeRunning = false;
+                            placeTrade();
+                        }, newTime);
+                    } else {
+                        isTradeRunning = false;
+                        placeTrade();
+                    }
+                } else {
+                    if (varObject.currentProfitAmount >= varObject.targetAmount) {
+                        let newTime = (getRandomNumber(2, 3) * 60000 );
+                        // let newTime = (getRandomNumber(2, 3) * 1000 );
+                        setTimer(newTime);
+                        setTimeout(() => {
+                            isTradeRunning = false;
+                            botRestart();
+                        }, newTime);
+                    } else {
+                        isTradeRunning = false;
+                        placeTrade();
+                    }
+
+                }
+                socket.send(JSON.stringify({msg_type: 'trade_closed',data: varObject}));
+
+
+            } else {
+                setTimeout(() => {
+                    socket.send(JSON.stringify({msg_type: 'tick_counter',data: {tick_count: contract.tick_count, length: contract.tick_stream.length}}));
+                    fetchTradeDetails(varObject.lastTradeDetails.id);
+                }, 1000);
+            }
+        }
 
     }
 
@@ -133,10 +201,25 @@ const getAuthentication = (data) => {
 
 
 const botStart = (data) => {
-    if (!isTradeRunning) {
+    botStartStatus = true;
+    botStopStatus = false;
+    if (!isTradeRunning && botStartStatus) {
         varObject.market = data.market
         placeTrade();
     }
+};
+
+const botRestart = () => {
+    if (!isTradeRunning && botStartStatus) {
+        placeTrade();
+    }
+};
+
+const botStop = () => {
+    botStartStatus = false;
+    botStopStatus = true;
+    isTradeRunning = false;
+    socket.send(JSON.stringify({msg_type: 'flash_message',data: {message: ``, time: 0}}));
 };
 
 function setInitData(data) {
@@ -146,6 +229,8 @@ function setInitData(data) {
 
         varObject.initianAccountBalance = Number(data.authorize.balance);
 
+        varObject.targetAmount =  Number((varObject.initianAccountBalance * (varObject.targetPercentage / 100)).toFixed(2));
+
         varObject.amountPutForTrading = Number((varObject.initianAccountBalance * (varObject.amountPercentage / 100)).toFixed(2));
         varObject.stake = Number(varObject.amountPutForTrading);
 
@@ -154,9 +239,32 @@ function setInitData(data) {
 }
 
 
+function setTimer(time) {
+    if(!botStopStatus){
+        let timeleft = time / 1000; // Convert milliseconds to seconds
+        if (!isTradeRunning && botStartStatus) {
+            timeleft = 0;
+            stopTimer = true;
+        }
+
+        let timer = setInterval(function () {
+            if (timeleft <= 0) {
+                clearInterval(timer);
+                socket.send(JSON.stringify({msg_type: 'flash_message',data: {message: ``, time: 0}}));
+            } else if (timeleft > 0 && !stopTimer) {
+                let formattedTime = formatTime(timeleft);
+                socket.send(JSON.stringify({msg_type: 'flash_message',data: {message: `Bot will run again in <span class="number">${formattedTime}</span>.`, time: 0}}));
+            }
+            timeleft -= 1;
+        }, 1000);
+    }
+
+}
+
+
 const placeTrade = (result = null) => {
     let tradeState = null;
-    if (isTradeRunning == false) {
+    if (!isTradeRunning && botStartStatus) {
         if (result != null) {
             if (result == "even") {
                 tradeState = "DIGITODD";
@@ -247,4 +355,83 @@ const fetchTradeDetails = (contractId) => {
     };
 
     ws.send(JSON.stringify(contractDetailsRequest));
+};
+
+// Helper function to format time (hide hours & minutes if they are 0)
+function formatTime(seconds) {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const secs = seconds % 60;
+
+    let timeString = "";
+
+    if (hours > 0) timeString += `${hours} h `;
+    if (minutes > 0) timeString += `${minutes} m `;
+    if (secs > 0 || timeString === "") timeString += `${secs} s`;
+
+    return timeString.trim();
+}
+
+
+
+const stakeChange = (status) => {
+    if (status == "Loss") {
+        varObject.stake = varObject.stake * martingaleMultiplier;
+    } else if (status == "Win") {
+        varObject.stake = varObject.amountPutForTrading;
+    }
+};
+
+const updateReturnDataObject = (lastTradeProfitValue) => {
+
+    varObject.totalTradeCount = varObject.totalTradeCount + 1;
+    varObject.updatedAccountBalance = varObject.updatedAccountBalance + lastTradeProfitValue;
+    varObject.netProfit = varObject.updatedAccountBalance - varObject.initianAccountBalance;
+
+    if(lastTradeProfitValue > 0){
+      
+        varObject.winTradeCount = varObject.winTradeCount + 1;
+        varObject.lostCountInRow = 0;
+        varObject.currentProfitAmount = varObject.currentProfitAmount + lastTradeProfitValue ;
+        varObject.currentLossAmount = 0 ;
+        
+    } else if(lastTradeProfitValue < 0){
+       
+        varObject.lossTradeCount = varObject.lossTradeCount + 1;
+        varObject.lostCountInRow = varObject.lostCountInRow + 1;
+
+        varObject.currentLossAmount = varObject.currentLossAmount + lastTradeProfitValue ;
+
+    }
+
+
+
+
+    // {
+    //     "initianAccountBalance": 533.01,
+    //     "amountPutForTrading": 0.53,
+    //     "targetAmount": 0.43,
+    //     "amountPercentage": 0.1,
+    //     "targetPercentage": 0.08,
+    //     "updatedAccountBalance": 533.01,
+    //     "stake": 0.53,
+    //     "market": null,
+    //     "totalTradeCount": 0,
+    //     "winTradeCount": 0,
+    //     "lossTradeCount": 0,
+    //     "lostCountInRow": 0,
+    //     "currentProfitAmount": 0,
+    //     "currentLossAmount": 0,
+    //     "netProfit": 0,
+    //     "lastTradeDetails": {
+    //         "id": null,
+    //         "type": null,
+    //         "market": null,
+    //         "stake": null,
+    //         "profit": null
+    //     }
+    // }
+
+
+
 };
